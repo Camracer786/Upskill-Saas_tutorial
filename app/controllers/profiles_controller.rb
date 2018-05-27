5358a4ccf28444bbb3c142ec8034434161616124
Ryan Jaffer <rjaffer@outlook.com>
2018-05-27 21:10:12 +0000

    Last stages of completion

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_current_user
   
   #GET to /user/:user_id/profile/new 
   def new
@@ -14,14 +16,41 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile( profile_params )
     if @profile.save
       flash[:success] = "Profile Updated!"
-      redirect_to user_path(params[:user_id])
+      redirect_to user_path(id: params[:user_id])
     else
       render action: :new
     end 
   end 
   
+  #Get requests made for editing profiles
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile 
+  end
+  
+  #This happens when someone makes a PUT Request on the Profile Edit page. 
+  def update
+    #Retrieve the user from the database
+    @user = User.find( params[:user_id] )
+    #Retreive that user's profile
+    @profile = @user.profile
+    #Mass assign edited profile attributes and update
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated"
+      #Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id])
+    else
+      render action: :edit
+    end
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
+    end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user! 
+  
+  def index
+    @users = User.includes(:profile)
+  end
   
   #When GET Request is sent for /users/:id
   def show
